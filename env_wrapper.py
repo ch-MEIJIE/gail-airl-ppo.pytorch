db35356ecbe194e407a32c5c43b14c752a284cb7
Jie Mei <45817951+ch-MEIJIE@users.noreply.github.com>
2024-07-03 22:45:12 +0800

    Add state normalization

--- env_wrapper.py
+++ env_wrapper.py
@@ -24,6 +24,9 @@ class PyFlytEnvWrapper:
         self.obs_bound_size = \
             self.env.observation_space["target_delta_bound"].shape[0]
         self._max_episode_steps = self.env.unwrapped.max_steps
+        self.obs_atti_Normalizer = Normalize(self.obs_atti_size)
+        self.obs_target_Normalizer = Normalize(self.obs_target_size)
+        self.obs_bound_Normalizer = Normalize(self.obs_bound_size)
 
     def reset(self):
         obs, _ = self.env.reset()
@@ -53,6 +56,37 @@ class PyFlytEnvWrapper:
         return obs, reward, done, info
 
     def concat_state(self):
+        # Normalize the states
+        self.state_atti = self.obs_atti_Normalizer(self.state_atti)
+        self.state_targ = self.obs_target_Normalizer(self.state_targ)
+        self.state_bound = self.obs_bound_Normalizer(self.state_bound)
         return np.concatenate(
             [self.state_atti, self.state_targ.flatten(), self.state_bound]
         )
+
+
+class Normalize:
+    def __init__(self, state_dim):
+        self.state_dim = state_dim
+        self.mean = np.zeros(state_dim)
+        self.std = np.zeros(state_dim)
+        self.stdd = np.zeros(state_dim)
+        self.n = 0
+
+    def __call__(self, x):
+        x = np.asarray(x)
+        self.n += 1
+        if self.n == 1:
+            self.mean = x
+        else:
+            old_mean = self.mean.copy()
+            self.mean = old_mean + (x - old_mean) / self.n
+            self.stdd = self.stdd + (x - old_mean) * (x - self.mean)
+        if self.n > 1:
+            self.std = np.sqrt(self.stdd / (self.n - 1))
+        else:
+            self.std = self.mean
+
+        x = x - self.mean
+        x = x / (self.std + 1e-8)
+        return x
